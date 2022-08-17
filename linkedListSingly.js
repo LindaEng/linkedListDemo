121465fecd5e12f0f113d600a3388917c7b68914
= <90lineng@gmail.com>
2022-08-17 16:29:07 -0400

    sll methods added

--- linkedListSingly.js
+++ linkedListSingly.js
@@ -10,7 +10,8 @@ class Node {
 //singly linkedList class
 class SinglyLinkedList {
     constructor() {
-        this.head 
+        this.head
+        this.tail 
     }
     setHead(data) {
         //check to see if there is an existing head
@@ -59,21 +60,92 @@ class SinglyLinkedList {
 //////////// Singly LL challenges /////////////
 
     //create a function that sets the tail(last node)
+    setTail(data) {
+        //we check to see if there is a head (there exist node(s))
+        if(!this.head) {
+            this.head = new Node(data)
+            this.tail = this.head
+        } else {
+            //we must traverse until we reach the last node [data: next: null]
+            let currentNode = this.head
+            while(currentNode.next !== null) {
+                currentNode = currentNode.next
+            }
+            //create a tempTail variable to host the old tail
+            let oldTail = currentNode // [data: next: null]
+            //redefine the old tail's next pointer to the new tail
+            oldTail.next = new Node(data)
+            this.tail = oldTail.next
+            return
+        }
+    }
 
     //find node
-    // findNode(data) {
+    findNode(data) {
+        //check to see if there exists a head
+        if(!this.head) {
+            console.log('No nodes in our Linked List')
+            return false
+        } else {
+            //traverse through our LL
+            let currentNode = this.head
+            //check to see if we are not at the end
+            while(currentNode !== null){
+                //check to see if the currentNode.data === data //return true return node
+                if(currentNode.data === data) {
+                    console.log('Node has been found', currentNode)
+                    return true
+                }
 
-    // }
+                currentNode = currentNode.next
+            }   
+            //return false
+            return false
+        }
+    }
 
     //insertNodeAtIdx
-    // insertNodeAtIdx(idx, node) {
+    insertNodeAtIdx(idx, data) {
+        //we are going to create counter, as we traverse through the list, we are going to increment the counter
+        //return the node that matches counter === idx
+        //check for the head
+        if(!this.head) {
+            return false
+        }else {
+            let counter = 0 // should clarify if the head is 1 or 0
+            //starting point
+            let currentNode = this.head
+            while(counter !== idx) {
+                //traverse && increment the counter
+                currentNode = currentNode.next
+                counter++
+            }
+            //preserve the currentNode.next node
+            let tempCurrNext = currentNode.next
+            //redefine the next pointer of currentNode to the newNode
+            currentNode.next = new Node(data)
+            // redefine the newNode's next to the preservered node 
+            let newNode = currentNode.next
+            newNode.next = tempCurrNext
 
-    // }
+        }
+    }
 
     //deleteNodeAtIdx
-    // deleteNodeAtIdx(idx){
-
-    // }
+    deleteNodeAtIdx(idx){
+        //check if there are nodes
+        if(!this.head) {
+            return false
+        } else {
+            let counter = 0
+            let currentNode = this.head
+            while(counter !== idx) {
+                currentNode = currentNode.next
+                counter++
+            }
+            
+        }
+    }
 
 //////////// CHALLENGE /////////////    
 //Prompt: Write a method that determines if the SSl is circular (hint: think of what happens when you draw a circle) //returns a boolean
